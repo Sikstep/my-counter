7949d863fabb9c3622864a5bec299e61075ec9e7
Sikstep <Sikstep1988@gmail.com>
2023-03-21 21:50:57 +0300

    tried include useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import s from './App.module.css';
 import {SuperButton} from './Components/SuperButton';
 import {InputMessanger} from './Components/InputMessanger';
 
 
+type State = {
+    count: number
+}
+
+type Action = { type: 'INC' } | { type: 'RESET' }
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'INC':
+            return {count: state.count + 1}
+        case 'RESET':
+            return {count: 0}
+        default:
+            return state
+    }
+}
 
 function App() {
 
     const startValue = 0;
     const maxValue = 5;
 
-    const [counter, setCounter] = useState<number>(startValue);
-    const [messages, setMessages] = useState<string[]>([]);
-    const incCount = () => {
-        if (counter < 5) {
-            setCounter(counter + 1)
-        }
-    }
+    // const [counter, setCounter] = useState<number>(startValue);
 
-    const resetCount = () => {
-        setCounter(0)
-    }
+    const [messages, setMessages] = useState<string[]>([]);
+
+    const [state, dispatch] = useReducer(reducer, {count: 0})
+
 
     const mappedMessages = messages.map((el, index) => {
         return (
-            <li key={index+1}>{el}</li>
+            <li key={index + 1}>{el}</li>
         )
     })
 
@@ -35,13 +46,21 @@ function App() {
         }
     }
 
+    function incCount() {
+        dispatch({type:'INC'})
+    }
+
+    function resetCount() {
+        dispatch({type:"RESET"})
+    }
+
     return (
         <div className="App">
             <div className={s.wrapper}>
-                <div className={counter === maxValue ? `${s.maxCount} ${s.counterBox}` : s.counterBox}>{counter}</div>
+                <div className={state.count === maxValue ? `${s.maxCount} ${s.counterBox}` : s.counterBox}>{state.count}</div>
                 <div className={s.buttonBox}>
-                    <SuperButton changeCount={incCount} disabled={counter === maxValue}>inc</SuperButton>
-                    <SuperButton changeCount={resetCount} disabled={counter === startValue}>reset</SuperButton>
+                    <SuperButton changeCount={incCount} disabled={state.count === maxValue}>inc</SuperButton>
+                    <SuperButton changeCount={resetCount} disabled={state.count === startValue}>reset</SuperButton>
                 </div>
             </div>
             <InputMessanger setMessage={setMessages} message={messages}/>
