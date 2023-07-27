33f9f0f8dd05a0afe3f3fdf7455cbde6ae6abdbb
Sikstep <Sikstep1988@gmail.com>
2023-07-27 18:28:25 +0300

    Create AppWithRedux.tsx

---
+++ src/AppWithRedux.tsx
@@ -0,0 +1,87 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import s from './App.module.css';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {MainCounter} from './Components/MainCounter';
+import {SettingForCounter} from './Components/SettingForCounter';
+
+type SettingsCountType = {
+    startValue: number
+    currentValue: number
+    maxValue: number
+}
+
+const stateKey = 'STATE_KEY'
+const saveLocalStorage = (key: string, data: SettingsCountType) => {
+    localStorage.setItem(key, JSON.stringify(data))
+}
+
+const getlocalStorage = (key: string) => {
+    let data = localStorage.getItem(key)
+    if (!data) return
+    return JSON.parse(data)
+}
+
+export function App() {
+
+    const initialState = {
+        startValue: 0,
+        currentValue: 0,
+        maxValue: 5,
+    }
+
+
+    const [settingsCount, setSettingsCount] = useState<SettingsCountType>(
+        getlocalStorage(stateKey) || initialState
+    );
+
+
+    useEffect(() => {
+        saveLocalStorage(stateKey, settingsCount)
+    }, [settingsCount])
+
+    const incriseCurCountHandler = () => {
+        setSettingsCount(prevState => ({...prevState, currentValue: settingsCount.currentValue + 1}))
+    }
+    const resetCountHandler = () => {
+        setSettingsCount(prevState => ({...prevState, currentValue: settingsCount.startValue}))
+    }
+    const changeStartValueHandler = (newStartValue: number) => {
+        // console.log('inputStartValue - ' + newStartValue)
+        setSettingsCount(prevState => ({...prevState, startValue: newStartValue}))
+        setSettingsCount(prevState => ({...prevState, currentValue: newStartValue}))
+    }
+    const changeMaxValueHandler = (newMaxValue: number) => {
+        // console.log('inputMaxValue - ' +newMaxValue)
+
+        setSettingsCount(prevState => ({...prevState, maxValue: newMaxValue}))
+    }
+
+    return (
+        <div className={s.app}>
+            <div className={s.wrapper}>
+                <Routes>
+                    <Route path={'/'} element={<Navigate to={'/settings'}/>}/>
+                    <Route path={'/counter'}
+                           element={<MainCounter
+                               maxValue={settingsCount.maxValue}
+                               currentValue={settingsCount.currentValue}
+                               incCount={incriseCurCountHandler}
+                               resetCount={resetCountHandler}
+
+                           />}/>
+                    <Route path={'/settings'} element={<SettingForCounter
+                        startValue={settingsCount.startValue}
+                        maxValue={settingsCount.maxValue}
+                        changeStartValue={changeStartValueHandler}
+                        changeMaxValue={changeMaxValueHandler}
+
+                    />}/>
+                </Routes>
+            </div>
+        </div>
+    );
+}
+
+
+
